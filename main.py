7f8d41a3a1bcb02e835dc167402a84f23c6cfed5
Scoprpibra <khateeb_i@hotmail.com>
2026-03-15 11:24:49 +0800

    main.py

--- main.py
+++ main.py
@@ -221,4 +221,5 @@ Transcript:
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT",3000)))
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
